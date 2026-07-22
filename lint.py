5968e722e2cdb450735bfb0a4676130cc5c6e20e
Tejeswini-18 <kollurilakshmitejes@cityuniversity.edu>
2026-07-21 17:37:26 -0700

    Updated lint.py

--- lint.py
+++ lint.py
@@ -1,15 +1,15 @@
-"""Generate and print the first ten Fibonacci numbers."""
+"The following code generate and print the Fibonacci numbers from 1 to 10."
 
 
 def fib(number):
-    """Return the nth Fibonacci number."""
+    "the following function returns the nth Fibonacci number."
     if number in (0, 1):
         return number
     return fib(number - 1) + fib(number - 2)
 
 
 def print_fib_sequence():
-    """Print the first ten Fibonacci numbers."""
+    "the following function Print the Fibonacci numbers from 1 to 10."""
     for i in range(1, 11):
         print(f"i: {i} fib: {fib(i - 1)}")
 
